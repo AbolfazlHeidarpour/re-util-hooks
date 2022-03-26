ad2b095f95a04f832fb2df7382977aeae08e3eae
AbolfazlHeidarpour <Abolfazl.heidarpour@gmail.com>
2022-03-26 13:29:12 +0430

    refactored useBoolean types

--- src/hooks/useBoolean/useBoolean.ts
+++ src/hooks/useBoolean/useBoolean.ts
@@ -1,35 +1,45 @@
-import { useCallback, useState } from "react";
+import React from "react";
 
-type TUseBoolean = {
+type TUseBoolean<T> = {
   state: boolean;
   toggle: () => void;
   reset: (condition?: boolean) => void;
   set: (condition?: boolean) =>  void;
+  onChange: (event: React.ChangeEvent<T>) => void;
 };
 
-const useBoolean = (initialValue: boolean = false): TUseBoolean => {
+const useBoolean = <T>(initialValue: boolean = false): TUseBoolean<T> => {
   // gets initial boolean value defaults to false and creates state...
-  const [state, setState] = useState<boolean>(initialValue);
+  const [state, setState] = React.useState<boolean>(initialValue);
 
   // toggles the boolean state...
-  const toggle = useCallback(
+  const toggle = React.useCallback(
     () => setState(!state),
-    []
+    [setState, state]
   );
 
-  const reset = useCallback(
+  const reset = React.useCallback(
     (condition?: boolean) =>
       setState(condition === undefined ? false : !condition),
-    []
+    [setState]
   );
 
-  const set = useCallback(
+  const set = React.useCallback(
     (condition?: boolean) =>
       setState(condition === undefined ? true : condition),
-    []
+    [setState]
+  );
+
+  const onChange = React.useCallback(
+    (event: React.ChangeEvent<T>) => {
+      if ('checked' in event.target)
+        setState(event.target['checked']);
+    },
+    [setState]
   );
 
   return {
+    onChange,
     state,
     toggle,
     reset,
